d6b8a2798e8d4b4e73204c04e5d8cb36417f831d
ricksterhd123 <ricksterhd123@gmail.com>
2019-01-08 03:04:41 +0000

    Image is showing, but test isn't passing

--- server.js
+++ server.js
@@ -10,8 +10,10 @@ app.get('/img/:width/:height',
     {
         let width = req.params.width
         let height = req.params.height
-        let invalid = height < 0 || height > 2000 || width  < 0 || width  > 20000
-        if (invalid) {res.send(403)} else {imager.sendImage(res, width, height)}
+        let invalid = height < 1 || height > 2000 || width  < 1 || width  > 2000
+        console.log(width)
+        console.log(height)
+        if (invalid) {res.sendStatus(403)} else {imager.sendImage(res, width, height, req.query.square, req.query.text)}
     }
 )
 app.listen(port)
