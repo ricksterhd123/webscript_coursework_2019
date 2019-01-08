631ff3079b1a96b4d324385741d46f384bc4ac60
ricksterhd123 <ricksterhd123@gmail.com>
2019-01-08 02:34:15 +0000

    Added image request handling

--- server.js
+++ server.js
@@ -5,5 +5,13 @@ const app = express()
 const port = 8080
 
 app.use('/', express.static('public'))
-app.get('/', function (req, res) { res.send('wew') })
+app.get('/img/:width/:height',
+    function (req, res)
+    {
+        let width = req.params.width
+        let height = req.params.height
+        let invalid = height < 0 || height > 2000 || width  < 0 || width  > 20000
+        if (invalid) {res.send(403)} else {imager.sendImage(res, width, height)}
+    }
+)
 app.listen(port)
